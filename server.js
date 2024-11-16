4dae2734ea88094d28656ed323a56dd5959f25a6
stefano <stefano.g.magni@hotmail.it>
2024-11-16 06:59:04 +0100

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -30,16 +30,25 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  /* myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {*/
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, null);
-  /*});*/
+  });
 });
   
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+    app.route('/').get((req, res) => {
+      res.render('index', { title: 'Hello', message: 'Please log in' });
+    });
+
+    }).catch(e => {
+      app.route('/').get((req, res) => {
+        res.render('index', { title: e, message: 'Unable to connect to database' });
+      });
 });
 
 const PORT = process.env.PORT || 3000;

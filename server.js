37d8de5ba541f9808d8c94bb6a15c1d1002202fc
stefano <stefano.g.magni@hotmail.it>
2024-11-16 07:39:44 +0100

    Implement the Serialization of a Passport User 5

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
@@ -10,25 +9,6 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-async function main(callback) {
-  const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
-  const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-
-      // Make the appropriate DB calls
-      await callback(client);
-
-  } catch (e) {
-      // Catch any errors
-      console.error(e);
-      throw new Error('Unable to Connect to Database')
-  }
-}
-
-module.exports = main;
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));

bca6b02cf1098aff29a97492e72e911173efbe70
stefano <stefano.g.magni@hotmail.it>
2024-11-16 08:48:24 +0100

    How to Use Passport Strategies

--- server.js
+++ server.js
@@ -33,10 +33,20 @@ myDB(async client => {
 
   app.route('/').get((req, res) => {
     res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please log in'
+      //title: 'Connected to Database',
+      //message: 'Please log in',
+      showLogin: true
     });
   });
+
+  app.route('/login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
+  res.redirect('profile');
+  });
+
+  app.route('/profile').get((req, res) =>{
+    res.render('profile');
+  });
+
 
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {

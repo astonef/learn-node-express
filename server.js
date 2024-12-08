06b196600df33d4496f46e635f1ce4d875449df4
stefano <stefano.g.magni@hotmail.it>
2024-12-08 10:57:55 +0100

    Hashing Your Passwords

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -53,8 +54,8 @@ myDB(async client => {
     res.redirect('/');
   });
 
-  app.route('/register')
-  .post((req, res, next) => {
+  app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
         next(err);
@@ -63,7 +64,7 @@ myDB(async client => {
       } else {
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         },
           (err, doc) => {
             if (err) {
@@ -95,7 +96,9 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`);
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
+      if (!bcrypt.compareSync(password, user.password)) { 
+          return done(null, false);
+      }
       return done(null, user);
     });
   }));

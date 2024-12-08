55072f94fe370cc31e74e4d452c4e9f50c007de5
stefano <stefano.g.magni@hotmail.it>
2024-12-08 10:30:07 +0100

    Registration of New Users 2

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
-const LocalStrategy = require('passport-local')
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -40,25 +40,17 @@ myDB(async client => {
     });
   });
 
-  app.route('/login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
-  res.redirect('profile');
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
   });
-  
-  app
-  .route('/profile')
-  .get(ensureAuthenticated, (req,res) => {
+
+  app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render('profile', { username: req.user.username });
   });
 
   app.route('/logout').get((req, res) => {
     req.logout();
     res.redirect('/');
-  });
-
-  app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
   });
 
   app.route('/register')
@@ -82,49 +74,56 @@ myDB(async client => {
               next(null, doc.ops[0]);
             }
           }
-        ); 
+        )
       }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
     });
   });
 
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
 
-   passport.use(new LocalStrategy((username, password, done) => {
-     myDataBase.findOne({ username: username }, (err, user) => {
-       console.log(`User ${username} attempted to log in.`);
-       if (err) return done(err);
-       if (!user) return done(null, false);
-       if (password !== user.password) return done(null, false);
-       return done(null, user);
-     });
-   }));
- 
-   passport.serializeUser((user, done) => {
-     done(null, user._id);
-   });
-   
-   passport.deserializeUser((id, done) => {
-     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-       done(null, doc);
-     });
-   });
- 
- }).catch(e => {
-   app.route('/').get((req, res) => {
-     res.render('index', { title: e, message: 'Unable to connect to database' });
-   });
- });
-
- function ensureAuthenticated(req, res, next) {
+function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/');
-  };
-   
- const PORT = process.env.PORT || 3000;
- app.listen(PORT, () => {
-   console.log(`Listening on port ${PORT}`);
- });
-
-
-
+};
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});

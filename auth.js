ec86cacfcc9010b81b77203988f8862bbcfe581a
stefano <stefano.g.magni@hotmail.it>
2024-12-14 11:46:04 +0100

    Implementation of Social Authentication II 1

--- auth.js
+++ auth.js
@@ -1,19 +1,21 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
+const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
       done(null, user._id);
   });
+
   passport.deserializeUser((id, done) => {
       myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
           if (err) return console.error(err);
           done(null, doc);
       });
   });
+
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
@@ -27,13 +29,13 @@ module.exports = function (app, myDataBase) {
   }));
 
   passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK
-  },
-  function (accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    //Database logic here with callback containing our user object
-  }
-));
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: 'https://learn-node-express-icry.onrender.com'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing our user object
+    }
+  ));
 }
